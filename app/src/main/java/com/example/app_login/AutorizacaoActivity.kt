f241d022620aa16605c4a603226fd575d0703638
Clayton Brito <clayton.brito115@gmail.com>
2026-06-15 23:59:12 -0300

    Feat:Criando a tela de boas vindas

--- app/src/main/java/com/example/app_login/AutorizacaoActivity.kt
+++ app/src/main/java/com/example/app_login/AutorizacaoActivity.kt
@@ -2,11 +2,16 @@ package com.example.app_login
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class AutorizacaoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +26,24 @@ class AutorizacaoActivity : AppCompatActivity() {
 
         val toolBarAutozicao = findViewById<Toolbar>(R.id.toolbarAutorizacao)
         setSupportActionBar(toolBarAutozicao)
-        supportActionBar?.title = "Autorização"
+        supportActionBar?.title = "Autorizar visitante"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        val dataVisita = findViewById<EditText>(R.id.edtDataVisita)
+
+        dataVisita.setOnClickListener {
+            val picker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Selecione a data da visita")
+                .build()
+
+            picker.show(supportFragmentManager, "DATE_PICKER")
+
+            picker.addOnPositiveButtonClickListener { selection ->
+                val format = SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR"))
+                dataVisita.setText(format.format(Date(selection)))
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
